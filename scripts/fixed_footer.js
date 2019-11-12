1a4919e4f005ccbe1a953901c9dbbd3ee5b8d6ec
pavkrol <pdkrol@gmail.com>
2019-11-12 15:19:57 +0100

    fixed some issues on mobile view (slideshow, fixed nav)

--- scripts/fixed_footer.js
+++ scripts/fixed_footer.js
@@ -4,21 +4,19 @@ const link = document.getElementById("contact_link");
 const nav = document.getElementById("navigation");
 const nav_position = nav.getBoundingClientRect();
 
-window.addEventListener("scroll", (e) => {
+window.addEventListener("scroll", e => {
   const scroll_position = window.scrollY;
   const screen_size = window.innerHeight - 100;
-  if(scroll_position > screen_size) {
+  if (scroll_position > screen_size) {
     link.classList.add("link_fixed");
     logo.classList.add("fixed");
-  }
-  else {
+  } else {
     link.classList.remove("link_fixed");
     logo.classList.remove("fixed");
   }
-  if(scroll_position > nav_position.top) {
+  if (scroll_position > nav_position.top && window.innerWidth > 800) {
     nav.classList.add("fixed");
-  }
-  else {
+  } else {
     nav.classList.remove("fixed");
   }
-})
+});
